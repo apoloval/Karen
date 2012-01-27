7a2a27366187c64cff01e17f73b22a8f97fcedbd
Alvaro Polo <apoloval@gmail.com>
2012-01-27 15:07:48 +0100

    Stream classes become templates to support generic streaming of objects.

--- include/utils/stream.h
+++ include/utils/stream.h
@@ -33,44 +33,48 @@ namespace karen { namespace utils {
  * Input stream class. This class provides an abstraction for an input stream,
  * this is, an object which allows reading bytes from it.
  */
+template <class T>
 class InputStream
 {
 public:
 
    /**
-    * Read len bytes from stream and write them in data. If there was a problem
+    * Read one element from the stream and return it. If there was a problem
     * while reading, a InvalidStateException is thrown.
     */
-   virtual unsigned long read(void* data, unsigned long len) 
+   virtual T read() throw (InvalidStateException) = 0;
+
+   /**
+    * Read len elements from stream and write them in data array. If there 
+    * was a problem while reading, a InvalidStateException is thrown.
+    */
+   virtual unsigned long read(T* data, unsigned long len) 
          throw (InvalidStateException) = 0;
    
-   /**
-    * Close the input stream and release any associated system resources.
-    */
-   virtual void close() = 0;
-
 };
 
 /**
  * Output stream class. This class provides an abstraction for an output stream,
  * this is, an object which allows writting bytes to it.
  */
+template <class T>
 class OutputStream
 {
 public:
 
    /**
-    * Write len bytes stored in data to this stream and return the number of
+    * Write one element to the stream. If there was a problem
+    * while reading, a InvalidStateException is thrown.
+    */
+   virtual void write(const T& data) throw (InvalidStateException) = 0;
+
+   /**
+    * Write len elements stored in data to this stream and return the number of
     * bytes actually written. If there was a problem while writing, a 
     * InvalidStateException is thrown.
     */
-   virtual unsigned long write(const void* data, unsigned long len) 
+   virtual unsigned long write(const T* data, unsigned long len) 
       throw (InvalidStateException) = 0;
-
-   /**
-    * Close the input stream and release any associated system resources.
-    */
-   virtual void close() = 0;
 
 };
 
