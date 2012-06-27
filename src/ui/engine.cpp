eec02b92918d7ac86430e912e9b006cbbd044cf2
Alvaro Polo <apoloval@gmail.com>
2012-06-27 20:02:47 +0200

    Dates on copyright note of all files updated.

--- src/ui/engine.cpp
+++ src/ui/engine.cpp
@@ -2,7 +2,7 @@
  * ---------------------------------------------------------------------
  * This file is part of Karen
  *
- * Copyright (c) 2007-2011 Alvaro Polo
+ * Copyright (c) 2007-2012 Alvaro Polo
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
