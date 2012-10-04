ec5c0f757ecd7fde90e5c1b8af9a5a34eb4fd9ba
Alvaro Polo <apoloval@gmail.com>
2012-10-04 14:44:47 +0200

    Remove unused class UnitTestSuite.

--- core/src/test.cpp
+++ core/src/test.cpp
@@ -118,69 +118,6 @@ UnitTest::addTest(Test* test)
    _tests[_nextTest++] = test;
 }
 
-UnitTestSuite::UnitTestSuite(const String& suiteName)
- : _name(suiteName),
-   _testCount(0)
-{
-}
-
-void
-UnitTestSuite::addTest(Test t)
-{ _test[_testCount++] = t; }
-
-unsigned int
-UnitTestSuite::run(
-      UnitTestReporter* rep,
-      TestResult* tstv, 
-      unsigned int tstc)
-{
-   if (rep) rep->beginUnitTestSuite(_name);
-   int i = 0, passed = 0;
-   while (i < _testCount)
-   {
-      if (rep) rep->beginUnitTest(_test[i].name);
-      if (tstv) tstv[i].test = _test[i];
-      try
-      {
-         (this->*_test[i].code)();
-         if (rep) rep->endUnitTest(TEST_RESULT_PASSED);
-         if (tstv) tstv[i].status = TEST_RESULT_PASSED;
-         passed++;
-      }
-      catch (InvalidAssertionException &e)
-      {
-         if (rep) rep->endUnitTest(TEST_RESULT_ASSERTION_FAILED, e.cause());
-         if (tstv) tstv[i].status = TEST_RESULT_ASSERTION_FAILED;
-      }
-      catch (TestFailedException &e)
-      {
-         if (rep) rep->endUnitTest(TEST_RESULT_FAILED, e.cause());
-         if (tstv) tstv[i].status = TEST_RESULT_FAILED;
-      }
-      catch (Exception& e)
-      {
-         if (rep) rep->endUnitTest(TEST_RESULT_FAILED, String::format(
-               "unexpected exception: %s", (const char*) e.cause()));
-         if (tstv) tstv[i].status = TEST_RESULT_FAILED;
-      }
-      catch (std::exception& e)
-      {
-         if (rep) rep->endUnitTest(TEST_RESULT_FAILED, String::format(
-               "unexpected exception: %s", e.what()));
-         if (tstv) tstv[i].status = TEST_RESULT_FAILED;
-      }
-      catch (...)
-      {
-         if (rep) rep->endUnitTest(
-               TEST_RESULT_FAILED, String("unknown exception raised"));
-         if (tstv) tstv[i].status = TEST_RESULT_FAILED;
-      }
-      i++;
-   }
-   if (rep) rep->endUnitTestSuite(passed, i);
-   return tstv ? i : 0;
-}
-
 void
 StdOutUnitTestReporter::beginUnitTestSuite(
       const String& suiteName)
